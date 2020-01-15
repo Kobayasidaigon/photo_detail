35998323a2511f64b2f99206e1a016536c42267b
Kobayashidaigon <hal.60445.kobayashi@gmail.com>
2020-01-15 11:44:56 +0900

    レーダーチャート表示できた

--- components/VueChart.vue
+++ components/VueChart.vue
@@ -1,70 +1,37 @@
 <script>
-import { Line } from "vue-chartjs";
-
+import { Radar } from "vue-chartjs";
 export default {
-  extends: Line,
-  data() {
-    return {
-      datacollection: {
-        //Data to be represented on x-axis
-        labels: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
-          "August",
-          "September",
-          "October",
-          "November",
-          "December"
-        ],
+  extends: Radar,
+  mounted() {
+    this.renderChart(
+      {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
         datasets: [
           {
-            label: "Data One",
-            backgroundColor: "#f87979",
-            pointBackgroundColor: "white",
-            borderWidth: 1,
-            pointBorderColor: "#249EBF",
-            //Data to be represented on y-axis
-            data: [40, 20, 30, 50, 90, 10, 20, 40, 50, 70, 90, 100]
+            label: "# of Votes",
+            data: [12, 0, 3, 4, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)"
+            ],
+            borderWidth: 0
           }
         ]
       },
-      //Chart.js options that controls the appearance of the chart
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true
-              },
-              gridLines: {
-                display: true
-              }
-            }
-          ],
-          xAxes: [
-            {
-              gridLines: {
-                display: false
-              }
-            }
-          ]
-        },
-        legend: {
-          display: true
-        },
-        responsive: true,
-        maintainAspectRatio: false
-      }
-    };
-  },
-  mounted() {
-    //renderChart function renders the chart with the datacollection and options object.
-    this.renderChart(this.datacollection, this.options);
+      { responsive: true, maintainAspectRatio: false }
+    );
   }
 };
 </script>
