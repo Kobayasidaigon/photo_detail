d3b3df33e7fc897e72f7c134168a5e1f89052b7e
Kobayashidaigon <hal.60445.kobayashi@gmail.com>
2020-01-21 01:30:53 +0900

    必要な記述

--- pages/index.vue
+++ pages/index.vue
@@ -31,9 +31,10 @@
 <script>
 import Chart from "../components/VueChart";
 import axios from "axios";
-const KEY = process.env.API;
+const KEY = "AIzaSyCVOiQfzqwYai-ecVlvAhlRyG_BS61pKas";
 const url = "https://vision.googleapis.com/v1/images:annotate?key=";
 const api_url = url + KEY;
+console.log(KEY);
 export default {
   data() {
     return {
